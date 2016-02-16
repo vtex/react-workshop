4dfe74322fb62f1be050a042ee09eff69396e0b6
resendefreitas <guilherme.freitas@vtex.com.br>
2016-02-16 21:27:46 -0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Olar mundo</h1>
+      </div>
+    );
+  }
+}
+
+export default App;

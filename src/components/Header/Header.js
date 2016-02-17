e64ca687f5fef83ee10e4b6ef661b234143077ef
resendefreitas <guilherme.freitas@vtex.com.br>
2016-02-17 14:25:19 -0200

    WOW! A COMPONENT

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.less';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div className="Header">
+        <h1>Olar, sou um header</h1>
+      </div>
+    );
+  };
+}
+
+export default Header;
